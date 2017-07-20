f5d466b7de13043dec6715611613715f4fe0be95
godspeed77 <godspeed77@126.com>
2017-07-21 02:55:18 +0800

    文章列表

--- views.py
+++ views.py
@@ -1,4 +1,23 @@
-from django.http import HttpResponse
+'''from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World")
+'''
+
+from django.shortcuts import render
+from block.models import Block
+
+'''
+def index(request):
+    return render(request,'cssindex.html')
+
+def index(request):
+    block_infos =[{"name":"运维专区", "desc":"讨论区", "manager":"liquan"},
+                  {"name":"django专区", "desc":"讨论区", "manager":"admin"},
+                  {"name":"部落建设", "desc":"事宜", "manager":"admin"}]
+    return render(request,"cssindex.html",{"blocks":block_infos})
+'''
+
+def index(request):
+    block_infos = Block.objects.filter(status=0).order_by("-id")
+    return render(request,"cssindex.html",{"blocks":block_infos})
